2e5aa4583747b34f07e8f262884f072446eacb12
DESKTOP-38HQMAO\hi <gullivervenom@gmail.com>
2024-10-31 16:19:27 +0900

    1031

---
+++ chap01-life-cycle-lecture-source/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JSP - Hello World</title>
+</head>
+<body>
+  <h1>서블릿의 라이프사이클 테스트
+    <br>
+    <a href="xml-lifecycle">xml 방식의 서블릿 라이프 사이클 테스트</a>
+  </h1>
+  <hr>
+  <a href="annotation-lifecycle">annotation 방식의 서블릿 라이프 사이클 테스트</a>
+</body>
+</html>
---
+++ chap06-excepntion-lecture-source/src/main/java/com/ohgiraffers/excepntion/Show500Servlet.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.excepntion;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/show500")
+public class Show500Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.sendError(500,"ㄱㅐ발자의 실수 ㅋㅋ");
+    }
+}
